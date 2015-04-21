af1480d78337002bdbf2fb1afe5207e808cf8823
azsivara <azsivara@cisco.com>
2015-04-21 16:12:29 -0700

    Adding cron scripts

---
+++ cron-hourly-ci-summary-report.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+timestamp() {
+   date +"%Y-%m-%d_%H-%M-%S"
+}
+
+cron_log_file="/tmp/cron-hourly-ci-summary-report-$(timestamp).txt"
+ci_daily_report_home="/apps/dftjenkins/workspace/ci-daily-reports"
+echo "Changing directory to $ci_daily_report_home..."
+cd $ci_daily_report_home
+source /apps/dftjenkins/workspace/bootstrap/bin/activate
+python ci-summary-report.py -f /apps/dftjenkins/JENKINS_HOME/jobs -sd 'today' -ct 'hourly' -p 'Snz1ylE0pxf!' >  $cron_log_file 2>&1
+
